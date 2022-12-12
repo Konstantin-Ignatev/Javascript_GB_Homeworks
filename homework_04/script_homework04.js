8711e874829e024b9fd926d3efcaef42c9e896d8
Константин Игнатьев <ignatyev.k@yandex.ru>
2022-12-12 22:03:51 +0300

    HW_04 done

---
+++ homework_04/script_homework04.js
@@ -0,0 +1,37 @@
+/*Задача 1*/
+
+// function isEven(a) {
+//     return a % 2 === 0;
+// }
+
+// for (let i = 0; i < 11; i++) {
+//     if (i === 0) {
+//         console.log(`${i} - это ноль`);
+//     } else if (isEven(i)) {
+//         console.log(`${i} - четное число`);
+//     } else {
+//         console.log(`${i} - нечетное число`);
+//     }
+// }
+
+/*Задача 2*/
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// arr.splice(3, 2)
+
+// console.log(arr);
+
+// /*Задача 5*/
+
+// const arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
+// console.log(`количество цифр в массиве ${arr.length}`);
+
+
+/*Задача 6*/
+
+// const arr = [1, 2, 3, 4, 5];
+
+// arr.splice(1, 2)
+
+// console.log(arr);
