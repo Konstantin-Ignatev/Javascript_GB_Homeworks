7a5cc96596c28a3c8cd112c1890dc59be27dbac0
Константин Игнатьев <ignatyev.k@yandex.ru>
2022-12-23 16:19:07 +0300

    HW_10 starting

---
+++ homework_10/data.js
@@ -0,0 +1,19 @@
+const dataJson = `[{
+        "quote_id": "1",
+        "quote": "I am not in danger, Skyler. I am the danger!",
+        "author": "Walter White",
+        "series": "Breaking Bad"
+    },
+    {
+        "quote_id": "2",
+        "quote": "Stay out of my territory.",
+        "author": "Walter White",
+        "series": "Breaking Bad"
+    },
+    {
+        "quote_id": "3",
+        "quote": "IFT",
+        "author": "Walter White",
+        "series": "Breaking Bad"
+    }
+]`
